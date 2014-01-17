d92a6d70bfc09da3f6b25461ab58c5def5b9e952
Phil Elson <pelson.pub@gmail.com>
2014-01-17 15:40:45 +0000

    Added v0.10.0 docs.

---
+++ cartopy/docs/v0.10/crs/projections-2.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+plt.figure(figsize=(6, 3))
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180))
+ax.coastlines(resolution='110m')
+ax.gridlines()
