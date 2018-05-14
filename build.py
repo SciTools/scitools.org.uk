746974718fb25f9c3bc60680d61d883cb7d51011
Carroll, Luke <luke.carroll@metoffice.gov.uk>
2018-05-14 17:01:33 +0100

    Redesign of scitools.org.uk based on a proof-of-concept using Mobirise.
    * Redirects in place for pages removed.
    * Stylised 404 handler.
    * Simplify the navigation, and remove the version information for each of
      the projects (that is now the responsibility of the project itself).
    * Pages are built on bootstrap and are entirely reactive.
    * Significant review from @pelson and @kaedonkers.

--- build.py
+++ build.py
@@ -1,33 +1,63 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import jinja2
 import os
+import re
 
 
-def render(source_dir, template_dir, build_dir):
-    if not os.path.exists(build_dir):
-        os.makedirs(build_dir)
-    loader = jinja2.FileSystemLoader([source_dir, template_dir])
-    template_env = jinja2.Environment(loader=loader)
+START_PATTERN = re.compile(r'(\s*)<!-- START ([A-Z\-]+) -->')
 
-    # Go through each of the source files.
-    for root, dir, files in os.walk(source_dir):
-        rel_root = os.path.relpath(root, source_dir)
-        for file in files:
-            template_name = os.path.join(rel_root, file)
-            # For tidyness, remove any extraneous path components (such
-            # as "/./" or "/../").
-            template_name = os.path.normpath(template_name)
-            output_name = os.path.join(build_dir, template_name)
-            print('{} --> {}'.format(template_name, output_name))
-            if not os.path.exists(os.path.dirname(output_name)):
-                os.makedirs(os.path.dirname(output_name))
-            with open(output_name, 'w') as fh:
-                template = template_env.get_template(template_name)
-                fh.write(template.render().encode('utf-8'))
+
+def main():
+    here = os.path.dirname(__file__)
+    master = 'index.html'
+    slaves = [os.path.join(here, page) for page in ['404.html', 'organisation.html', 'privacy.html']]
+
+    blocks = {}
+    with open(os.path.join(here, master), 'r') as fh:
+        for line in fh:
+            match = START_PATTERN.match(line)
+            if match:
+                indent = match.group(1)
+                block_name = START_PATTERN.match(line).group(2)
+                next_line = line
+                lines = [line,
+                         "{}<!-- DO NOT MODIFY THIS BLOCK DIRECTLY. CHANGE {} THEN RE-BUILD -->\n".format(indent, master)]
+                while not next_line.strip().startswith('<!-- END {} -->'.format(block_name)):
+                     next_line = next(fh)
+                     lines.append(next_line)
+                blocks[block_name] = lines
+
+    for slave in slaves:
+        insert_block(slave, blocks)
+
+
+def insert_block(slave, blocks):
+    with open(slave, 'r') as fh:
+        lines = list(fh)
+    lines_iter = iter(lines)
+    new_lines = []
+
+    for line in lines_iter:
+        match = START_PATTERN.match(line)
+        if match:
+            block_name = match.group(2)
+            if block_name not in blocks:
+                 raise ValueError(
+                      "Looking for a {} block, but it doesn't "
+                      "exist".format(block_name))
+            for new_line in blocks[block_name]:
+                new_lines.append(new_line)
+            while not next(lines_iter).strip().startswith('<!-- END {} -->'.format(block_name)):
+                pass
+        else:
+            new_lines.append(line)
+
+    if lines != new_lines:
+        with open(slave, 'w') as fh:
+            for line in new_lines:
+                fh.write(line)
 
 
 if __name__ == '__main__':
-    here = os.path.dirname(__file__)
-    render(os.path.join(here, 'source'), os.path.join(here, 'templates'), './')
+    main()
