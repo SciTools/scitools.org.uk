14372c8b678feeceead7d2e4f124de9e187481a7
Carroll, Luke <luke.carroll@metoffice.gov.uk>
2018-05-24 15:32:35 +0100

    Add a build script that simplifies templating of standard html.

--- build.py
+++ build.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import jinja2
 import os
 
 
-def render(source_dir, template_dir, build_dir):
-    if not os.path.exists(build_dir):
-        os.makedirs(build_dir)
-    loader = jinja2.FileSystemLoader([source_dir, template_dir])
-    template_env = jinja2.Environment(loader=loader)
+def main():
+    here = os.path.dirname(__file__)
+    master = os.path.join(here, 'index.html')
+    slaves = [os.path.join(here, page) for page in ['404.html']]
 
-    # Go through each of the source files.
-    for root, dir, files in os.walk(source_dir):
-        rel_root = os.path.relpath(root, source_dir)
-        for file in files:
-            template_name = os.path.join(rel_root, file)
-            # For tidyness, remove any extraneous path components (such
-            # as "/./" or "/../").
-            template_name = os.path.normpath(template_name)
-            output_name = os.path.join(build_dir, template_name)
-            print('{} --> {}'.format(template_name, output_name))
-            if not os.path.exists(os.path.dirname(output_name)):
-                os.makedirs(os.path.dirname(output_name))
-            with open(output_name, 'w') as fh:
-                template = template_env.get_template(template_name)
-                fh.write(template.render().encode('utf-8'))
+    blocks = {}
+    with open(master, 'r') as fh:
+        for line in fh:
+            if line.strip().startswith('<!-- START'):
+                next_line = line
+                lines = []
+                while not next_line.strip().startswith('<!-- END'):
+                     lines.append(next_line)
+                     next_line = next(fh)
+                lines.append(next_line)
+                blocks['STYLES'] = lines
+    
+    for slave in slaves:
+        insert_block(slave, blocks)
+    
+
+def insert_block(slave, blocks):
+    with open(slave, 'r') as fh:
+        lines = list(fh)
+    lines_iter = iter(lines)
+    new_lines = []
+
+    for line in lines_iter:
+        if line.strip().startswith('<!-- START'):
+            for new_line in blocks['STYLES']:
+    	        new_lines.append(new_line)
+    	    while not next(lines_iter).strip().startswith('<!-- END'):
+    	        pass
+        else:
+    	    new_lines.append(line)
+
+    if lines != new_lines:   
+        with open(slave, 'w') as fh:
+            for line in new_lines:
+                fh.write(line)
 
 
 if __name__ == '__main__':
-    here = os.path.dirname(__file__)
-    render(os.path.join(here, 'source'), os.path.join(here, 'templates'), './')
+    main()
+
