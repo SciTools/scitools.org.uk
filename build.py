229ce56b82f25f82b4c013d53abc217ad8dc4ffa
Carroll, Luke <luke.carroll@metoffice.gov.uk>
2018-05-24 15:52:53 +0100

    Added a styles block to template CSS includes. More block needed later.

--- build.py
+++ build.py
@@ -2,25 +2,30 @@
 from __future__ import print_function
 
 import os
+import re
+
+
+START_PATTERN = re.compile(r'\s*<!-- START ([A-Z]+) -->')
 
 
 def main():
     here = os.path.dirname(__file__)
-    master = os.path.join(here, 'index.html')
-    slaves = [os.path.join(here, page) for page in ['404.html']]
+    master = 'index.html'
+    slaves = [os.path.join(here, page) for page in ['404.html', 'organisation.html', 'privacy.html']]
 
     blocks = {}
-    with open(master, 'r') as fh:
+    with open(os.path.join(here, master), 'r') as fh:
         for line in fh:
-            if line.strip().startswith('<!-- START'):
+            if START_PATTERN.match(line):
+                block_name = START_PATTERN.match(line).group(1)
                 next_line = line
-                lines = []
-                while not next_line.strip().startswith('<!-- END'):
-                     lines.append(next_line)
+                lines = [line,
+                         "  <!-- DO NOT MODIFY THIS BLOCK DIRECTLY. CHANGE {} THEN RE-BUILD -->\n".format(master)]
+                while not next_line.strip().startswith('<!-- END {} -->'.format(block_name)):
                      next_line = next(fh)
-                lines.append(next_line)
-                blocks['STYLES'] = lines
-    
+                     lines.append(next_line)
+                blocks[block_name] = lines
+
     for slave in slaves:
         insert_block(slave, blocks)
     
@@ -32,10 +37,15 @@ def insert_block(slave, blocks):
     new_lines = []
 
     for line in lines_iter:
-        if line.strip().startswith('<!-- START'):
-            for new_line in blocks['STYLES']:
+        if START_PATTERN.match(line):
+            block_name = START_PATTERN.match(line).group(1)
+            if block_name not in blocks:
+                 raise ValueError(
+                      "Looking for a {} block, but it doesn't "
+                      "exist".format(block_name))
+            for new_line in blocks[block_name]:
     	        new_lines.append(new_line)
-    	    while not next(lines_iter).strip().startswith('<!-- END'):
+    	    while not next(lines_iter).strip().startswith('<!-- END {} -->'.format(block_name)):
     	        pass
         else:
     	    new_lines.append(line)
